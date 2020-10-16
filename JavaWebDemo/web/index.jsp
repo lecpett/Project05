1a159c7a36b57314d34d98361a2eccaf3052d2f3
lecpett <1870562577@qq.com>
2020-10-16 10:35:12 +0800

    Initial commit

---
+++ JavaWebDemo/web/index.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: liwang
+  Date: 2020/10/15
+  Time: 22:25
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <h1 style="color: red;">
+    nihao
+  </h1>
+  </body>
+</html>
---
+++ src/org/lw/p1/HelloWorld.java
@@ -0,0 +1,28 @@
+package org.lw.p1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        /**
+         * 快捷键的使用
+         * ctrl+d复制
+         * shift+回车切到下一行
+         */
+
+        /**
+         * 便捷模板
+         * 模板的设置位置在settings-Editor-live Templates /postfixCompletion
+         * 1、sout：System.out.println();
+         *      aaa.sout：System.out.println("aaa");
+         * 2、ifn：if (args == null)
+         * 3、inn：if (args != null)
+         * 4、psvm：main方法
+         * 5、fori：for循环
+         * 6、xxx.forr：倒叙循环
+         */
+        System.out.println("II");
+    }
+
+    public static void m(){
+
+    }
+}
